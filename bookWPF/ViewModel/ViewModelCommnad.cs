586db7680453687417b2a07f4e62b6560422cc9d
bokob <bmc0520@naver.com>
2023-06-19 21:04:54 +0900

    SQL이랑 로그인 연결중

---
+++ bookWPF/ViewModel/ViewModelCommnad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace bookWPF.ViewModel
+{
+    public class ViewModelCommnad : ICommand
+    {
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
+
+        // 생성자
+        public ViewModelCommnad(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+
+        public ViewModelCommnad(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null?true:_canExecuteAction(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
